2b97d564f2f398aa1733435c7bd949aaae3def5c
Ann Pru <anna.pru.prudnikova@yandex.by>
2023-03-13 15:42:18 +0300

    init push

---
+++ ConsoleApplication2.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+int Date(int D, int M, int Y)
+{
+    int a, y, m, R;
+    a = (14 - M) / 12;
+    y = Y - a;
+    m = M + 12 * a - 2;
+    R = 7000 + (D + y + y / 4 - y / 100 + y / 400 + (31 * m) / 12);
+    return R % 7;
+}
+
+int main()
+{
+    setlocale(LC_ALL, "Rus");
+    int D, M, Y;
+    char C;
+    string S[7] = { "Воскресенье", "Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота" };
+    cout << "Введите дд/мм/гг"<< endl;
+    cin >> D >> C >> M >> C >> Y;
+
+    cout << S[Date(D, M, Y)] << endl;
+
+    system("pause");
+    return 0;
+}
